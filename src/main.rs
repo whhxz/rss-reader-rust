38da2cdf910868d9480f219f48324e25800fad2b
whhxz <xuzhuowhh@gmail.com>
2024-04-22 23:04:24 +0800

    add web and template

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
+use askama::Template;
+use axum::{
+    extract,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router,
+};
+use serde_json::json;
 pub mod model;
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello World!" }))
+        .route("/json", get(|| async { Json(json!({ "name": "whhxz" })) }))
+        .route(
+            "/html/:name",
+            get(|extract::Path(name): extract::Path<String>| async {
+                let t = HelloTemplate { name };
+                match t.render() {
+                    Ok(html) => Html(html).into_response(),
+                    Err(err) => (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Failed to render template. Error: {err}"),
+                    )
+                        .into_response(),
+                }
+            }),
+        );
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:6789")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
 }
